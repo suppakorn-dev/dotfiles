44ce96b2fae4dcbb477798c2990f8474da17d918
Bas Suppakorn <suppakorn.tam@ascendcorp.com>
2023-10-24 08:46:06 +0700

    feat: remapping nvim key

--- home/private_dot_config/nvim/lua/custom/mappings.lua
+++ home/private_dot_config/nvim/lua/custom/mappings.lua
@@ -1,7 +1,20 @@
 local M = {}
 
+M.disabled = {
+  n = {
+    ["<C-u>"] = "",
+    ["<C-d>"] = "",
+    ["n"] = "",
+    ["N"] = "",
+  }
+}
+
 M.dap = {
   n = {
+    ["n"] = {"nzzzv", "Place cursor at the middle of page when navigate on search result"},
+    ["N"] = {"Nzzzv", "Place cursor at the middle of page when navigate on search result"},
+    ["<C-u>"] = {"<C-u>zz", "Go up half page and place the cursor at the middle of page"},
+    ["<C-d>"] = {"<C-d>zz", "Go down half page and place the cursor at the middle of page"},
     ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>", "Add breakpoint at line"},
     ["<leader>dr"] = {"<cmd> DapContinue <CR>", "Run or continue the debugger"}
   },
